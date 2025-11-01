c689e7f2a5aa728e95f141b76df1a0ebd0f9f0a2
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-01 15:56:11 +0530

    Cleanup unused stuff

--- core/presets.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-from core.utils import TextStyle, get_path
-
-pygame.font.init()
-# def_font = pygame.font.SysFont("D:\Python Projects\Pygame Stuff\Dew Valley\graphics\LycheeSoda.ttf")
-overlay_style = TextStyle(
-    "white", font_name=get_path(r"..\graphics\LycheeSoda.ttf")
-)
