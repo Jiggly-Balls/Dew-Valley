64a766d6806d51fcf247158cacd54623bc98823f
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-25 19:47:00 +0530

    Added game loop methods

--- states/base.py
+++ states/base.py
@@ -6,8 +6,12 @@ from game_state import State
 from game_state.utils import MISSING
 
 if TYPE_CHECKING:
-    from pygame import Surface
+    from pygame import Event, Surface
 
 
 class BaseState(State["BaseState"]):
     window: Surface = MISSING
+
+    def process_event(self, event: Event) -> None: ...
+
+    def process_update(self, dt: float) -> None: ...
