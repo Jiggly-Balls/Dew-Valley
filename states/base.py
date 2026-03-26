796e3ecbb4840751cef17e6045e0eab684509da0
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-26 13:02:52 +0530

    Added process_event functionality

--- states/base.py
+++ states/base.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pygame
 from game_state import State
 from game_state.utils import MISSING
 
@@ -9,9 +10,14 @@ if TYPE_CHECKING:
     from pygame import Event, Surface
 
 
+__all__ = ("BaseState",)
+
+
 class BaseState(State["BaseState"]):
     window: Surface = MISSING
 
-    def process_event(self, event: Event) -> None: ...
+    def process_event(self, event: Event) -> None:
+        if event.type == pygame.QUIT:
+            self.manager.is_running = False
 
     def process_update(self, dt: float) -> None: ...
