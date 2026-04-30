420cc075184c666041ec4b81297cc7ea2f89819c
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-01 01:33:53 +0530

    Fixed type errors in sky.py

--- entities/sky.py
+++ entities/sky.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import random
+from typing import TYPE_CHECKING
 
 import pygame
 
@@ -6,21 +9,35 @@ from core.settings import *
 from core.utils import import_folder
 from entities.sprites import BaseSprite
 
+if TYPE_CHECKING:
+    from typing import Any, Literal
+
+    from pygame import Surface, Vector2
+    from pygame.sprite import Group
+
+    from entities.player import CameraGroup
+
 
 class Sky:
-    def __init__(self, display: pygame.Surface) -> None:
-        self.display_surface = display
-        self.full_surf = pygame.Surface(Display.SCREEN_RESOLUTION)
-        self.start_color = [255, 255, 255]
-        self.end_color = (38, 101, 189)
-        self.day_speed = 1.2
+    def __init__(self, display: Surface) -> None:
+        self.display_surface: Surface = display
+        self.full_surf: Surface = pygame.Surface(Display.SCREEN_RESOLUTION)
+        self.start_color: list[int | float] = [255, 255, 255]
+        self.end_color: tuple[Literal[38], Literal[101], Literal[189]] = (
+            38,
+            101,
+            189,
+        )
+        self.day_speed: float = 1.2
 
     def display(self, dt: float) -> None:
         for index, value in enumerate(self.end_color):
             if self.start_color[index] > value:
                 self.start_color[index] -= self.day_speed * dt
 
-        self.full_surf.fill(self.start_color)
+        self.full_surf.fill(
+            pygame.Color(*(int(channel) for channel in self.start_color))
+        )
         self.display_surface.blit(
             self.full_surf, (0, 0), special_flags=pygame.BLEND_RGBA_MULT
         )
@@ -30,22 +47,22 @@ class Drop(BaseSprite):
     def __init__(
         self,
         surf: pygame.Surface,
-        pos: Tuple[int, int],
+        pos: tuple[int, int],
         moving: bool,
-        groups: Union[pygame.sprite.Group, Sequence[pygame.sprite.Group]],
+        groups: Group[Any] | Sequence[Group[Any]],
         z: int,
     ) -> None:
         # general setup
         super().__init__(pos, surf, groups, z)
-        self.lifetime = random.randint(400, 500)
-        self.start_time = pygame.time.get_ticks()
+        self.lifetime: int = random.randint(400, 500)
+        self.start_time: int = pygame.time.get_ticks()
 
         # moving
-        self.moving = moving
+        self.moving: bool = moving
         if self.moving:
-            self.pos = pygame.math.Vector2(self.rect.topleft)
-            self.direction = pygame.math.Vector2(-2, 4)
-            self.speed = random.randint(200, 250)
+            self.pos: Vector2 = pygame.math.Vector2(self.rect.topleft)
+            self.direction: Vector2 = pygame.math.Vector2(-2, 4)
+            self.speed: int = random.randint(200, 250)
 
     def update(self, dt: int) -> None:
         # movement
@@ -60,12 +77,19 @@ class Drop(BaseSprite):
 
 class Rain:
     def __init__(
-        self, display: pygame.Surface, all_sprites: pygame.sprite.Group
+        self, display: pygame.Surface, all_sprites: CameraGroup
     ) -> None:
-        self.display_window = display
-        self.all_sprites = all_sprites
-        self.rain_drops = import_folder("graphics/images/rain/drops")
-        self.rain_floor = import_folder("graphics/images/rain/floor")
+        self.display_window: Surface = display
+        self.all_sprites: CameraGroup = all_sprites
+        self.rain_drops: list[Surface] = import_folder(
+            "graphics/images/rain/drops"
+        )
+        self.rain_floor: list[Surface] = import_folder(
+            "graphics/images/rain/floor"
+        )
+        self.floor_w: int
+        self.floor_h: int
+
         self.floor_w, self.floor_h = pygame.image.load(
             "graphics/images/world/ground.png"
         ).get_size()
